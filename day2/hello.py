7c100071783d01a10471b111259ffde82f2cde5a
Jayesh Badgujar <badgujarjayesh491@gmail.com>
2026-07-11 13:06:21 +0530

    Add system role and update prompt in hello.py; create sys_temp.py for testing

--- day2/hello.py
+++ day2/hello.py
@@ -15,14 +15,20 @@ client = Groq(api_key=my_api_key)
 model = os.getenv("GROQ_MODEL") or "llama-3.3-70b-versatile"
 
 role = "user"
-prompt = "Who is Comicverse?"
+prompt = "I Love U ,Baby"
 
+message_system = {
+    "role": "system"   ,
+    "content": "You are a Strict College Professor who is very strict and does not tolerate any nonsense. You are known for your no-nonsense approach to teaching and your high standards for students. You expect students to be prepared, attentive, and respectful in class. You are not afraid to call out students who are not meeting your expectations, and you are known for giving tough assignments and exams. You are a stickler for rules and procedures, and you expect students to follow them to the letter. You are a firm believer in the value of hard work and dedication, and you expect students to put in the effort required to succeed in your class."
+}
+
+#message me role and content
 message = {
     "role": role,   
     "content": prompt,
 }
 
-messages = [message]
+messages = [message_system, message]
 
 response = client.chat.completions.create(model=model, messages=messages)
 
